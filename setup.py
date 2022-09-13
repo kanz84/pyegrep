65588bbe9733794cb752f744dfc175d29d2bd4ac
kanz84 <kanz1384@gmail.com>
2022-09-13 12:07:09 +0430

    doc

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ from distutils.core import setup
 
 setup(
     name="pyegrep",
-    version="1.0.3",
+    version="1.0.5",
     packages=["pyegrep"],
     url="https://github.com/kanz84/pyegrep",
     license="MIT",
     author="kanz84",
     author_email="kanz1384@gmail.com",
     description="A tool for grep",
-    download_url="https://github.com/kanz84/pyegrep/archive/refs/tags/v1.0.2.tar.gz",
+    download_url="https://github.com/kanz84/pyegrep/archive/refs/tags/v1.0.5.tar.gz",
     keywords=["grep", "egrep"],
     install_requires=[],
     classifiers=[
