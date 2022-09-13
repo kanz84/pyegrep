18a3fb1f26b8b3c46c516c65b0fc0fcbfd73437c
kanz84 <kanz1384@gmail.com>
2022-09-13 09:18:49 +0430

    add setup

---
+++ setup.py
@@ -0,0 +1,28 @@
+from distutils.core import setup
+
+setup(
+    name="pyegrep",
+    version="1.0.0",
+    packages=["pyegrep"],
+    url="https://github.com/kanz84/pyegrep",
+    license="MIT",
+    author="kanz84",
+    author_email="kanz1384@gmail.com",
+    description="A tool for grep",
+    download_url="https://github.com/kanz84/pyegrep/archive/refs/tags/v1.0.0.tar.gz",
+    keywords=["grep", "egrep"],
+    install_requires=[],
+    classifiers=[
+        "Development Status :: 1 - Alpha",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Build Tools",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+    ],
+)
